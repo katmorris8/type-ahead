f5de458f39314ea3a9d2f39826b85958cca75326
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-04-22 20:46:58 -0400

    added event listeners to the search input

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function findMatches(wordToMatch, cities) {
         return place.city.match(regex) || place.state.regex;
     })
 }
+
+function displayMatches() {
+    console.log(this.value);
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);  
